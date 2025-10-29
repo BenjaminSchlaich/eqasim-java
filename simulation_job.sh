d93bc4579095c4328c5388aeffc949ab884bb96b
Benjamin Schlaich <benjamin.schlaich@icloud.com>
2025-10-29 13:03:42 +0100

    huiiii

--- simulation_job.sh
+++ simulation_job.sh
@@ -14,7 +14,11 @@ module load gcc/12.2.0
 module load openjdk/21.0.3_9
 module load maven
 
-cd /cluster/scratch/bschlaich/eqasim-java
+cd /cluster/scratch/bschlaich/eqasim-java/switzerland
 # mvn --offline -DskipTests clean package
-mvn --offline exec:java -Dexec.mainClass="org.eqasim.switzerland.ch_cmdp.RunSimulation" -Dexec.args="--config-path /cluster/scratch/bschlaich/eqasim-java/Zurich_10pct/zurich_10pct_config.xml"
+# mvn --offline exec:java -Dexec.mainClass="org.eqasim.switzerland.ch_cmdp.RunSimulation" -Dexec.args="--config-path /cluster/scratch/bschlaich/eqasim-java/Zurich_10pct/zurich_10pct_config.xml"
 #  
+mvn exec:java \
+  -Dexec.mainClass=org.eqasim.switzerland.ch_cmdp.RunSimulation \
+  -Dexec.args="--config-path /Users/benjaminschlaich/Coding/MatSim/eqasim-java/Zurich_10pct/zurich_10pct_config.xml" \
+  -Dexec.classpathScope=compile
