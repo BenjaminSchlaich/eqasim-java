d947f2eb948f1d4c6c2bace448556d4b483743ec
Benjamin Schlaich <benjamin.schlaich@icloud.com>
2025-10-29 13:16:52 +0100

    offline script lol

--- simulation_job.sh
+++ simulation_job.sh
@@ -18,7 +18,7 @@ cd /cluster/scratch/bschlaich/eqasim-java/switzerland
 # mvn --offline -DskipTests clean package
 # mvn --offline exec:java -Dexec.mainClass="org.eqasim.switzerland.ch_cmdp.RunSimulation" -Dexec.args="--config-path /cluster/scratch/bschlaich/eqasim-java/Zurich_10pct/zurich_10pct_config.xml"
 #  
-mvn exec:java \
+mvn --offline exec:java \
   -Dexec.mainClass=org.eqasim.switzerland.ch_cmdp.RunSimulation \
   -Dexec.args="--config-path /cluster/scratch/bschlaich/eqasim-java/Zurich_10pct/zurich_10pct_config.xml" \
   -Dexec.classpathScope=compile
