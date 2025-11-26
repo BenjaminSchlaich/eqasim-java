30d776730c207cc38bf627c77a35a2e2585fa034
Leopold Frieberger <frieberger.leopold@outlook.com>
2025-11-26 13:42:29 +0100

    Added instruction for running compiled on euler

---
+++ running_compiled_on_euler/instructions for local compilation.md
@@ -0,0 +1,19 @@
+Make sure that locally, the inner pom file has this line:
+```
+<mainClass>org.eqasim.switzerland.zurich.RunSimulation</mainClass>
+```
+Which specifies the main class to be used when running the jar. You can of course change this to any other main class you want to run.
+
+First, compile the project with:
+```
+module load stack/2024-06 openjdk/21.0.3_9
+```
+
+This gives you multiple jar files in the inner `target` folder. Then copy the `switzerland-2.0.0.jar` file to the cluster.
+
+Also have the scenario on the cluster (at `scenarios/Zurich_10pct`) and move the jar file to the same folder as the config file, because there is a buy that prevents the locating of the .csv and .yml files otherwise.
+
+From `scratch/<your_user>` run the following command to submit the job:
+```
+batch < jar_job.sh 
+```
---
+++ running_compiled_on_euler/jar_job.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#SBATCH --job-name=matsim_simulation    #Name of the job
+#SBATCH --ntasks=1                      #Requesting 1 node (is always 1)
+#SBATCH --cpus-per-task=8               #Requesting 8 CPU
+#SBATCH --mem-per-cpu=16G                #Requesting 3 Gb memory per core, 24 Gb in total 
+#SBATCH --time=6:00:00                  #Requesting 6 hours run time
+
+#Source the GDC stack
+# source /cluster/project/gdc/shared/stack/GDCstack.sh
+
+#Load the needed modules
+# module load stack/2024-06
+# module load gcc/12.2.0
+# module load openjdk/21.0.3_9
+# module load maven
+
+module load stack/2024-06 openjdk/21.0.3_9
+
+cd scenarios/Zurich_10pct
+
+
+# mvn --offline -DskipTests clean package
+# mvn --offline exec:java -Dexec.mainClass="org.eqasim.switzerland.ch_cmdp.RunSimulation" -Dexec.args="--config-path /cluster/scratch/bschlaich/eqasim-java/Zurich_10pct/zurich_10pct_config.xml"
+#  
+java -jar switzerland-2.0.0.jar --config-path zurich_10pct_config.xml
