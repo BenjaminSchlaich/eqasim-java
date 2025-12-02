6410f45fd3bea50707ee2722face97ed9ac7e666
Benjamin Schlaich <benjamin.schlaich@icloud.com>
2025-12-02 14:16:23 +0100

    Fixed BikePredictor

--- core/src/main/java/org/eqasim/core/simulation/mode_choice/utilities/predictors/BikePredictor.java
+++ core/src/main/java/org/eqasim/core/simulation/mode_choice/utilities/predictors/BikePredictor.java
@@ -11,6 +11,8 @@ import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceTrip;
 public class BikePredictor extends CachedVariablePredictor<BikeVariables> {
 	@Override
 	public BikeVariables predict(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> elements) {
+
+		// XX .get(2) because the bike trip is at index 2 in the chain walk-bikeinteraction-bike-bikeinteraction-walk
 		double travelTime_min = ((Leg) elements.get(0)).getTravelTime().seconds() / 60.0;
 
 		return new BikeVariables(travelTime_min);
