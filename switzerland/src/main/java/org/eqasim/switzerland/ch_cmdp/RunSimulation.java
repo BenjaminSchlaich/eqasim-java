e5dfaf2087e1e2d97204b274ef1d7c7e139ed6d8
Leopold Frieberger <frieberger.leopold@outlook.com>
2026-01-15 14:01:06 +0100

    Next simulation run. This one is the big one, where everyone gets a bikes  on network, but it is still filtered to be just those with FOR_ALL or FOR_SOME thatcan use them

--- switzerland/src/main/java/org/eqasim/switzerland/ch_cmdp/RunSimulation.java
+++ switzerland/src/main/java/org/eqasim/switzerland/ch_cmdp/RunSimulation.java
@@ -47,7 +47,7 @@ public class RunSimulation {
 				.requireOptions("config-path") //
 				.allowPrefixes("mode-parameter", "cost-parameter", "preventwaitingtoentertraffic", "samplingRateForPT") //
 				// XX these are our prefixes:
-				.allowPrefixes("add-bike-vehicles", "bike-bias", "car-bias", "pt-bias", "population-wealth-factor", "network-speed-factor", "population-bike-chance", "early-exit", "remove-elevation", "redistribute-bikes", "bike-max-velocity", "car-penalty-per-km") //
+				.allowPrefixes("add-bike-vehicles", "bike-bias", "car-bias", "pt-bias", "population-wealth-factor", "slow-traffic", "population-bike-chance", "early-exit", "remove-elevation", "redistribute-bikes", "bike-max-velocity", "car-penalty-per-km") //
 				.build();
 
 		SwitzerlandConfigurator configurator = new SwitzerlandConfigurator(cmd);
@@ -148,11 +148,9 @@ public class RunSimulation {
 			logger.info("Setting population wealth factor to " + wealthFactor);
 		}
 
-		if (cmd.hasOption("network-speed-factor")) {
-			double speedFactor = Double.parseDouble(cmd.getOption("network-speed-factor").get());
+		if (cmd.hasOption("slow-traffic")) {
 			NetworkConfigurator networkConfigurator = new NetworkConfigurator();
-			networkConfigurator.applySpeedFactor(scenario.getNetwork(), speedFactor);
-			logger.info("Setting network speed factor to " + speedFactor);
+			networkConfigurator.turn50to30(scenario.getNetwork());
 		}
 
 		if (cmd.hasOption("population-bike-chance")) {
